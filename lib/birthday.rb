7243ab3a419ad2fcfa5e058c280330bb136e720d
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-19 02:42:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,5 +15,6 @@ def age_appropiate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end 
   end
 end
