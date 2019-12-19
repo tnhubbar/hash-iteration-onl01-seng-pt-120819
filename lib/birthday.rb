5228deb1bfa5d4e158e2f4171bd291e30819a0ba
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-19 02:39:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
-    
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
